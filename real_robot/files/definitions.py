032af55709105a0446afceaa76801df33b083bd6
lucianobonzatto <37905647+lucianobonzatto@users.noreply.github.com>
2024-01-09 11:28:14 +0100

    update definitions

--- real_robot/files/definitions.py
+++ real_robot/files/definitions.py
@@ -9,18 +9,22 @@ ENC2_B = 23  # blue
 PULSES_PER_TURN = 96  # Number of encoder pulses per revolution of the wheel
 
 # MOTOR DRIVER: supply to GND and Vin
-ML_A = 26   # A-1A  Left motor +
-ML_B = 25   # A-1B  Left Motor -
-MR_A = 16   # B-1B  Right Motor -
-MR_B = 17   # B-1A  Right Motor +
+
+EN_LEFT = 26         # A-1A  Left enable
+POSITIVE_LEFT = 17   # B-1B  Left Motor -
+NEGATIVE_LEFT = 16   # B-1A  Left Motor +
+
+EN_RIGHT = 25         # A-1B  Right enable
+POSITIVE_RIGHT = 2   # B-1B  Right Motor -
+NEGATIVE_RIGHT = 4   # B-1A  Right Motor +
 
 SOLENOID_PIN = 13  # connect to Magnet board (supply the board with GND and +5V)
 TOUCHSW_pin = 12   # switch it to ground
 DELTA_T_ms = 40    # Cycle time in milliseconds
 
 # IR line sensor: Connect to GND and 3V3
-IR1 = 32    # Blue   IR1 IO33 - I've switched IR1 and IR2 because connections are inverted in my robot!
-IR2 = 33    # Yellow IR2 IO32 - I've switched IR1 and IR2 because connections are inverted in my robot!
+IR1 = 33    # Blue   IR1 IO33 - I've switched IR1 and IR2 because connections are inverted in my robot!
+IR2 = 32    # Yellow IR2 IO32 - I've switched IR1 and IR2 because connections are inverted in my robot!
 IR3 = 39    # Orange IR3 IO39
 IR4 = 36    # Green  IR4 IO36
 IR5 = 34    # White  IR5 IO34
@@ -38,3 +42,4 @@ RX = 9      # UART-RX pin
 # Robot Size
 Wheel_Radius = 0.0205
 Whell_Distance = 0.0565
+
--- real_robot/test_main.py
+++ real_robot/test_main.py
@@ -11,6 +11,7 @@ from files.rotary_irq_esp import RotaryIRQ
 from files.WiFiInterface import WiFiInterface
 from files.robot import My_Robot
 from files.definitions import *
+from files.utils import *
 
 import time
 
@@ -23,31 +24,33 @@ delta_t = DELTA_T_ms / 1000
 #######################################################
 
 ## L9110 motor driver
-from files.dcmotor import L9110 as DCMotor
-pinA1A = PWM(Pin(ML_A), frequency)
-pinA1B = PWM(Pin(ML_B), frequency)
-pinB1A = PWM(Pin(MR_A), frequency)
-pinB1B = PWM(Pin(MR_B), frequency)
-left_motor = DCMotor(pinA1A, pinA1B, 0, 1023)
-right_motor = DCMotor(pinB1A, pinB1B, 0, 1023)
+#from files.dcmotor import L9110 as DCMotor
+#pinA1A = PWM(Pin(ML_A), frequency)
+#pinA1B = PWM(Pin(ML_B), frequency)
+#pinB1A = PWM(Pin(MR_A), frequency)
+#pinB1B = PWM(Pin(MR_B), frequency)
+#left_motor = DCMotor(pinA1A, pinA1B, 0, 1023)
+#right_motor = DCMotor(pinB1A, pinB1B, 0, 1023)
+
+#######################################################
+## L298 motor driver
+
+from files.dcmotor import L298 as DCMotor
+enable_l = PWM(Pin(EN_LEFT), frequency)
+left_p = Pin(POSITIVE_LEFT, Pin.OUT)
+left_n = Pin(NEGATIVE_LEFT, Pin.OUT)
+
+enable_r = PWM(Pin(EN_RIGHT), frequency)
+right_p = Pin(POSITIVE_RIGHT, Pin.OUT)
+right_n = Pin(NEGATIVE_RIGHT, Pin.OUT)
+
+
+left_motor = DCMotor(left_p, left_n, enable_l, 0, 1023)
+right_motor = DCMotor(right_p, right_n, enable_r, 0, 1023)
+
 left_motor.stop()
 right_motor.stop()
-
-## solenoid driver
-solenoid = PWM(Pin(SOLENOID_PIN), frequency)	# define PWM for solenoid pin
-solenoid.duty(0)	# Turn off magnet solenoid
-solenoid_state = False
-
-## switch driver
-touchsw = Pin(TOUCHSW_pin, Pin.IN, Pin.PULL_UP)	# define touch switch pin with internal pull-up
-
-## line sensor driver
-IR1_pin = ADC(Pin(IR1))		# define ADC for line sensor pin IR1
-IR2_pin = ADC(Pin(IR2))		# define ADC for line sensor pin IR2
-IR3_pin = ADC(Pin(IR3))		# define ADC for line sensor pin IR3
-IR4_pin = ADC(Pin(IR4))		# define ADC for line sensor pin IR4
-IR5_pin = ADC(Pin(IR5))		# define ADC for line sensor pin IR5
-analog_pins = [IR1_pin, IR2_pin, IR3_pin, IR4_pin, IR5_pin]
+#######################################################
 
 ## encoder driver
 encoder_right = RotaryIRQ(pin_num_clk=ENC1_A, 
@@ -64,13 +67,35 @@ encoder_left = RotaryIRQ(pin_num_clk=ENC2_A,
               range_mode=RotaryIRQ.RANGE_WRAP)	# Left wheel encoder: 968 ppr
 encoders = [encoder_right, encoder_left]
 
+
+## switch driver
+touchsw = Pin(TOUCHSW_pin, Pin.IN, Pin.PULL_UP)	# define touch switch pin with internal pull-up
+
+## line sensor driver
+IR1_pin = ADC(Pin(IR1))		# define ADC for line sensor pin IR1
+IR2_pin = ADC(Pin(IR2))		# define ADC for line sensor pin IR2
+IR3_pin = ADC(Pin(IR3))		# define ADC for line sensor pin IR3
+IR4_pin = ADC(Pin(IR4))		# define ADC for line sensor pin IR4
+IR5_pin = ADC(Pin(IR5))		# define ADC for line sensor pin IR5
+
+IR1_pin.atten(ADC.ATTN_11DB)
+IR2_pin.atten(ADC.ATTN_11DB)
+IR3_pin.atten(ADC.ATTN_11DB)
+IR4_pin.atten(ADC.ATTN_11DB)
+IR5_pin.atten(ADC.ATTN_11DB)
+analog_pins = [IR1_pin, IR2_pin, IR3_pin, IR4_pin, IR5_pin]
+
+## solenoid driver
+solenoid = PWM(Pin(SOLENOID_PIN), frequency)	# define PWM for solenoid pin
+#solenoid.duty(1000)
+solenoid.duty(0)	# Turn off magnet solenoid
+
 #######################################################
 
 my_robot = My_Robot(analog_pins, left_motor, right_motor, encoders, touchsw, solenoid)
-wifi_interface = WiFiInterface(wifi_ssid, wifi_password, server_ip, server_port)
-wifi_interface.run()
+#wifi_interface = WiFiInterface(wifi_ssid, wifi_password, server_ip, server_port)
+#wifi_interface.run()
 
-#analog_read = ADC(Pin(33))
 #for i in range(6000):
 #    x = "A1 " + str(analog_read.read()) +";A2 100;loop 1;"
 #    wifi_interface.send_data(x.encode())
@@ -90,27 +115,18 @@ print("Starting...")
 
 while True:
     time.sleep(delta_t)
+    cycle_count += 1
+    led_board.value(not led_board.value())
     data = my_robot.update()
     
     data = data + "A1 " + str(cycle_count) + ";loop 1;"
+    #wifi_interface.send_data(data.encode())
+    
+    line_sensor_values = my_robot.readIRSensors()
+    touch_sw_value = my_robot.readSwitch()
+    my_robot.writeSolenoid(True)
+    print(touch_sw_value)
+    print(line_sensor_values)
     print(data)
-    wifi_interface.send_data(data.encode())
-
-    cycle_count += 1
-#    led_board.value(not led_board.value())
     
-#    # Update sensor readings
-#    line_sensor_values = my_robot.readIRSensors()
-#    touch_sw_value = my_robot.readSwitch()
     
-#    # Turn solenoid ON/OFF
-#    if led_board.value():
-#        solenoid.duty(1000)
-#        solenoid_state = True
-#    else:
-#        solenoid.duty(0)
-#        solenoid_state = False
-
-#    # Click the touch switch to turn motor test 
-#    if touch_sw_value:
-#        my_robot.test_motors()
