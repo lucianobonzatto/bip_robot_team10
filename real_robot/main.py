de8904beb764fbd9dc00d4e2f2ee31f19e786f02
lucianobonzatto <37905647+lucianobonzatto@users.noreply.github.com>
2024-01-09 18:13:54 +0100

    main

--- real_robot/main.py
+++ real_robot/main.py
@@ -6,7 +6,6 @@
 # You can modify and build on this code to complete your assignment.
 
 from machine import Pin, PWM, UART, ADC, Timer
-
 from files.rotary_irq_esp import RotaryIRQ
 from files.WiFiInterface import WiFiInterface
 from files.robot import My_Robot
@@ -35,32 +34,21 @@ delta_t = DELTA_T_ms / 1000
 ## L298 motor driver
 
 from files.dcmotor import L298 as DCMotor
-pin1 = Pin(21, Pin.OUT)
-pin2 = Pin(4, Pin.OUT)
-enable = PWM(Pin(22), frequency)
-enable2 = PWM(Pin(23), frequency)
-left_motor = DCMotor(pin1, pin2, enable, 0, 1023)
-right_motor = DCMotor(pin1, pin2, enable2, 0, 1023)
-#######################################################
+enable_l = PWM(Pin(EN_LEFT), frequency)
+left_p = Pin(POSITIVE_LEFT, Pin.OUT)
+left_n = Pin(NEGATIVE_LEFT, Pin.OUT)
+
+enable_r = PWM(Pin(EN_RIGHT), frequency)
+right_p = Pin(POSITIVE_RIGHT, Pin.OUT)
+right_n = Pin(NEGATIVE_RIGHT, Pin.OUT)
+
+
+left_motor = DCMotor(left_p, left_n, enable_l, 0, 1023)
+right_motor = DCMotor(right_p, right_n, enable_r, 0, 1023)
 
 left_motor.stop()
 right_motor.stop()
-
-## solenoid driver
-solenoid = PWM(Pin(SOLENOID_PIN), frequency)	# define PWM for solenoid pin
-solenoid.duty(0)	# Turn off magnet solenoid
-solenoid_state = False
-
-## switch driver
-touchsw = Pin(TOUCHSW_pin, Pin.IN, Pin.PULL_UP)	# define touch switch pin with internal pull-up
-
-## line sensor driver
-IR1_pin = ADC(Pin(IR1))		# define ADC for line sensor pin IR1
-IR2_pin = ADC(Pin(IR2))		# define ADC for line sensor pin IR2
-IR3_pin = ADC(Pin(IR3))		# define ADC for line sensor pin IR3
-IR4_pin = ADC(Pin(IR4))		# define ADC for line sensor pin IR4
-IR5_pin = ADC(Pin(IR5))		# define ADC for line sensor pin IR5
-analog_pins = [IR1_pin, IR2_pin, IR3_pin, IR4_pin, IR5_pin]
+#######################################################
 
 ## encoder driver
 encoder_right = RotaryIRQ(pin_num_clk=ENC1_A, 
@@ -77,102 +65,67 @@ encoder_left = RotaryIRQ(pin_num_clk=ENC2_A,
               range_mode=RotaryIRQ.RANGE_WRAP)	# Left wheel encoder: 968 ppr
 encoders = [encoder_right, encoder_left]
 
+
+## switch driver
+touchsw = Pin(TOUCHSW_pin, Pin.IN, Pin.PULL_UP)	# define touch switch pin with internal pull-up
+
+## line sensor driver
+IR1_pin = ADC(Pin(IR1))		# define ADC for line sensor pin IR1
+IR2_pin = ADC(Pin(IR2))		# define ADC for line sensor pin IR2
+IR3_pin = ADC(Pin(IR3))		# define ADC for line sensor pin IR3
+IR4_pin = ADC(Pin(IR4))		# define ADC for line sensor pin IR4
+IR5_pin = ADC(Pin(IR5))		# define ADC for line sensor pin IR5
+
+IR1_pin.atten(ADC.ATTN_11DB)
+IR2_pin.atten(ADC.ATTN_11DB)
+IR3_pin.atten(ADC.ATTN_11DB)
+IR4_pin.atten(ADC.ATTN_11DB)
+IR5_pin.atten(ADC.ATTN_11DB)
+analog_pins = [IR1_pin, IR2_pin, IR3_pin, IR4_pin, IR5_pin]
+
+## solenoid driver
+solenoid = PWM(Pin(SOLENOID_PIN), frequency)	# define PWM for solenoid pin
+#solenoid.duty(1000)
+solenoid.duty(0)	# Turn off magnet solenoid
+
 #######################################################
 
 my_robot = My_Robot(analog_pins, left_motor, right_motor, encoders, touchsw, solenoid)
-wifi_interface = WiFiInterface(wifi_ssid, wifi_password, server_ip, server_port)
-wifi_interface.run()
-analog_read = ADC(Pin(33))
+#wifi_interface = WiFiInterface(wifi_ssid, wifi_password, server_ip, server_port)
+#wifi_interface.run()
 
-for i in range(6000):
-    x = "A1 " + str(analog_read.read()) +";A2 100;loop 1;"
-    wifi_interface.send_data(x.encode())
-    data = wifi_interface.receive_data()
-    if data != None:
-        print("Dados Recebidos:", data)
-    time.sleep_ms(100)
+#for i in range(6000):
+#    x = "A1 " + str(analog_read.read()) +";A2 100;loop 1;"
+#    wifi_interface.send_data(x.encode())
+#    data = wifi_interface.receive_data()
+#    if data != None:
+#        print("Dados Recebidos:", data)
+#    time.sleep_ms(100)
     
 #######################################################
 
-# flag = False
-# motor_test = False
-
-# def timerCallback(timer):
-#     ''' Timer callback: executed every periodic timer interval
-#     '''
-#     global oldEncoderValues
-#     global left_encoder_value
-#     global right_encoder_value
-#     global cycle_count
+#print("Click the switch to start.")
+#while touchsw.value() == True:
+#    # Switch value is False when clicked
+#    time.sleep(0.1)
     
-#     encoderValues = [encoder_left.value(), encoder_right.value()]
-#     wl, wr = get_wheels_speed(encoderValues, oldEncoderValues, PULSES_PER_TURN, delta_t)
-#     oldEncoderValues = [left_encoder_value, right_encoder_value]
-#     left_encoder_value = encoder_left.value()
-#     right_encoder_value = encoder_right.value()
+print("Starting...")
+
+while True:
+    time.sleep(delta_t)
+    cycle_count += 1
+    led_board.value(not led_board.value())
+    data = my_robot.update()
+    my_robot.test_motors()
     
-#     # Print line sensor values
-#     print(line_sensor_values, left_encoder_value, right_encoder_value, cycle_count, wl, wr)
-
-#     led_board.value(not led_board.value())		# Invert board LED
-#     cycle_count = 0
-
-# print("Click the switch to start.")
-# while touchsw.value() == True:
-#     # Switch value is False when clicked
-#     sleep(0.1)
+    data = data + "A1 " + str(cycle_count) + ";loop 1;"
+    #wifi_interface.send_data(data.encode())
     
-# print("Starting...")
-# sleep(1)
-
-# # Initiate Timer
-# timer = Timer(1)
-# timer.init(period=DELTA_T_ms, mode=Timer.PERIODIC, callback = timerCallback)
-
-# while True:
-#     cycle_count += 1
+    line_sensor_values = my_robot.readIRSensors()
+    touch_sw_value = my_robot.readSwitch()
+    my_robot.writeSolenoid(True)
+    print(touch_sw_value)
+    print(line_sensor_values)
+    print(data)
     
-#     # Update sensor readings
-#     line_sensor_values = readIRSensors(analog_pins)
-#     touch_sw_value = touchsw.value()
     
-#     # Turn solenoid ON/OFF according to the value of solenoid_state
-#     if led_board.value():
-#         solenoid.duty(1000)
-#         solenoid_state = True
-#     else:
-#         solenoid.duty(0)
-#         solenoid_state = False
-
-#     # Click the touch switch to turn motor test ON/OFF
-#     if not touchsw.value() and not flag:
-#         flag = True
-#         motor_test = True    
-#     if touchsw.value() and flag:
-#         flag = False
-        
-#     # Run motor test sequence if motor_test is true
-#     if motor_test:
-#         # Stop the timer to execute motor test
-#         timer.deinit()
-#         print("\nMotor test. \n*** Hold the touch switch to cancel. ***\n")
-#         print("Initiating in... ", end='')
-#         sleep(1)
-#         print("3, ", end="")
-#         sleep(1)
-#         print("2, ", end="")
-#         sleep(1)
-#         print("1, ", end="")
-#         sleep(1)
-#         print("0.")
-#         if touchsw.value():
-#             test_motors(left_motor, right_motor, encoder_right, encoder_left)  # Test the motors
-#             left_motor.stop()
-#             right_motor.stop()
-#             print("Test finished. \n")
-#         else:
-#             print("Motor test aborted. \n")
-#             sleep(2)
-#         motor_test = False
-#         # Restart the timer
-#         timer.init(period=DELTA_T_ms, mode=Timer.PERIODIC, callback = timerCallback)
